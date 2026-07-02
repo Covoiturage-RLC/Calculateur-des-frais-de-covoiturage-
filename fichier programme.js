3f2f2097f4b0612e5891cecd7a6d03fe5f38a013
Covoiturage-RLC <photos.randoloisirsculture@gmail.com>
2026-07-02 23:22:21 +0200

    Update fichier programme.js
    Ajouté Math round

--- fichier programme.js
+++ fichier programme.js
@@ -79,7 +79,7 @@ btnValider.addEventListener("click", (e) => {
     prxKm.value = "";
   } else {
     ctTots.value =
-      Number(((disTot.value) * Number(prxKm.value)) + Number(peage.value)) * Number(nbVoit.value);
+     Math.round ((Number(((disTot.value) * Number(prxKm.value)) + Number(peage.value)) * Number(nbVoit.value)) * 100) / 100;
     ctTotv.value =
       Number(ctTots.value) / Number(nbVoit.value);
     ctBpp.value =
