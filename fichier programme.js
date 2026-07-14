07761b343bdff5a36a68567b1e7798da162abbb0
Covoiturage-RLC <photos.randoloisirsculture@gmail.com>
2026-07-14 11:56:30 +0200

    Update fichier programme.js

--- fichier programme.js
+++ fichier programme.js
@@ -91,7 +91,7 @@ btnValider.addEventListener("click", (e) => {
     Math.round (((Number(ctBpp.value)) - 12) * 100) / 100);
     ctRetp.value =
       Math.round (Number(ctBpp.value) - Number(ctDep12E.value));
-    debcagnoRLC.value =
+    debcagnoRLC.value = Math.abs
     (Number(ctTots.value) - (Number(ctRetp.value) * Number(nbPass.value)));
     crecagnoRLC.value =
     (Number(ctTots.value) - (Number(ctRetp.value) * Number(nbPass.value)));
