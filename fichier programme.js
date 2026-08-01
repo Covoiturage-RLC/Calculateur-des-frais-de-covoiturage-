f8e31517a3f563776615c99a725ce3672e126caa
Covoiturage-RLC <photos.randoloisirsculture@gmail.com>
2026-08-01 14:33:36 +0200

    Fix calculation for crecagnoRLC value

--- fichier programme.js
+++ fichier programme.js
@@ -93,7 +93,8 @@ btnValider.addEventListener("click", (e) => {
     ctRetp.value =
       Math.round (Number(ctRpp.value) - Number(ctDep12E.value));
     crecagnoRLC.value =
-      Math.abs (Number(ctTots.value) - (Number(ctRetp.value) * Number(nbPass.value))));
+      
+      Math.abs (Number(ctTots.value) - (Number(ctRetp.value) * Number(nbPass.value)));
     debcagnoRLC.value =
       Math.max(
         0, 
